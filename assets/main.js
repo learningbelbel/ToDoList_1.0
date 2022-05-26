a6aa50f31b8cc70839ec09270f80fe67b69af370
Abel Alvarez <learn.belbel@gmail.com>
2022-05-25 18:28:09 -0600

    javascript working

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,16 @@
 const botonAdd = document.getElementById("agregar")
 const addTarea = document.getElementById("add-tarea");
+const addsection = document.querySelector(".add")
+const closebtn = document.getElementById("close")
 
 botonAdd.addEventListener("click",()=>{
-    addTarea.classList.toggle("close")
+    addTarea.classList.add("open")
+    addsection.classList.add("close")
+})
+
+closebtn.addEventListener("click",()=>{
+    addTarea.classList.remove("open")
+    addsection.classList.remove("close")
+
+    
 })
